419cad9049e8ee30eef8793d97d37cb6f9cf0531
JohanGJensen <johancentralen@gmail.com>
2023-07-06 18:00:12 +0200

    add table for years

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,9 +1,53 @@
 <script>
-  import Calendar from '@johang/calendarjs';
+  import Calendar from "@johang/calendarjs";
   const calendar = new Calendar(1980, 2023);
+  const years = calendar.getYears();
 
-  console.log(calendar);
+  console.log(calendar, years);
 </script>
 
 <h1>Calendar package</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+  <table>
+    <th>year</th>
+    {#each years as { year }}
+      <tr>
+        {year}
+      </tr>
+    {/each}
+  </table>
+</div>
+
+<style>
+  div {
+    width: 500px;
+    height: 400px;
+    overflow-y: scroll;
+  }
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    width: 100%;
+  }
+
+  table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+
+  table tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  table tr:hover {
+    background-color: #ddd;
+  }
+
+  table th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #04aa6d;
+    color: white;
+  }
+</style>
